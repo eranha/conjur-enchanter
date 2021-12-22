62daaf59bee999c3722269544e4cb665ad0a3b5d
eranha <eran.hadar@cyberark.com>
2021-12-22 17:05:27 +0200

    Added ability to provide ok button approval

--- src/main/java/com/cyberark/views/ResourceFormView.java
+++ src/main/java/com/cyberark/views/ResourceFormView.java
@@ -2,12 +2,12 @@ package com.cyberark.views;
 
 import com.cyberark.Consts;
 import com.cyberark.models.Annotation;
-import com.cyberark.models.ResourceIdentifier;
 import com.cyberark.models.ResourceType;
 
 import java.awt.*;
 import java.beans.PropertyChangeListener;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Interfae to all resource forms used to populate a resource model.
@@ -20,5 +20,6 @@ public interface ResourceFormView extends Consts {
   void setPropertyChangeListener(PropertyChangeListener listener);
   Component getComponent();
   int showDialog(Window owner, String title);
+  int showDialog(Window owner, String title, Supplier<Boolean> enableOkButton);
   ResourceType getResourceType();
 }
