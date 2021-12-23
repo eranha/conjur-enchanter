b0d86f42770362b7a0aeff9f6811d6843b5ae622
eranha <eran.hadar@cyberark.com>
2021-12-24 01:06:46 +0200

    New icon

--- src/main/java/com/cyberark/actions/ReloadViewAction.java
+++ src/main/java/com/cyberark/actions/ReloadViewAction.java
@@ -1,14 +1,17 @@
 package com.cyberark.actions;
 
 import com.cyberark.controllers.ControllerFactory;
+import com.cyberark.views.Icons;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
+import static com.cyberark.Consts.CYBR_BLUE;
+
 public class ReloadViewAction extends AbstractAction {
   public ReloadViewAction() {
-    super("Reload");
+    super("Reload", Icons.getInstance().getIcon(Icons.ICON_SPIN, 16, CYBR_BLUE));
     putValue(SHORT_DESCRIPTION, "Reload view");
     putValue(MNEMONIC_KEY, KeyEvent.VK_R);
   }
