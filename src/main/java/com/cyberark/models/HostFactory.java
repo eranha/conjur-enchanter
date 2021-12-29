1082f044ae515113975dc26b45291ff6778c89ab
eranha <eran.hadar@cyberark.com>
2021-12-29 17:08:07 +0200

    New model

---
+++ src/main/java/com/cyberark/models/HostFactory.java
@@ -0,0 +1,32 @@
+package com.cyberark.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HostFactory extends ResourceModel {
+
+  private final String[] layers;
+  private final String[] tokens;
+
+  @JsonCreator
+public HostFactory(@JsonProperty("created_at") String createdAt,
+                     @JsonProperty("id") String id,
+                     @JsonProperty("owner") String owner,
+                     @JsonProperty("policy") String policy,
+                     @JsonProperty("permissions") Permission[] permissions,
+                     @JsonProperty("annotations") Annotation[] annotations,
+                     @JsonProperty("layers") String[] layers,
+                     @JsonProperty("tokens") String[] tokens) {
+    super(createdAt, id, owner, policy, permissions, annotations);
+    this.layers = layers;
+    this.tokens = tokens;
+  }
+
+  public String[] getLayers() {
+    return layers;
+  }
+
+  public String[] getTokens() {
+    return tokens;
+  }
+}
