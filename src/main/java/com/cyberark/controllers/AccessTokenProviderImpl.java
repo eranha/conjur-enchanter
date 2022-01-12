510f0b6927cff851d1284c7a5040295a50ddb6f9
scooter72 <eran.hadar@gmail.com>
2022-01-12 12:50:45 +0200

    Cache access token until it expires

--- src/main/java/com/cyberark/controllers/AccessTokenProviderImpl.java
+++ src/main/java/com/cyberark/controllers/AccessTokenProviderImpl.java
@@ -1,19 +1,28 @@
 package com.cyberark.controllers;
 
 import com.cyberark.Credentials;
+import com.cyberark.Util;
+import com.cyberark.exceptions.AuthenticationException;
 import com.cyberark.resource.Endpoints;
 import com.cyberark.resource.ResourceApiProvider;
-import com.cyberark.exceptions.AuthenticationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.function.Supplier;
 
 public class AccessTokenProviderImpl implements AccessTokenProvider {
+  private static final int DEFAULT_LIFESPAN_SECONDS = 8 * 60;
   private final ResourceApiProvider resourceApiProvider;
   private String endpoint;
   private char[] apiKey;
   private final Supplier<Credentials> credentialsSupplier;
+  private char[] token;
+  private Instant tokenExpiration = Instant.MIN;
 
   public AccessTokenProviderImpl(ResourceApiProvider resourceProvider,
                                  Supplier<Credentials> credentialsSupplier) {
@@ -27,15 +36,31 @@ public class AccessTokenProviderImpl implements AccessTokenProvider {
       setEndpoint(credentialsSupplier.get());
     }
 
+    if (token != null && tokenExpiration.isAfter(Instant.now())) {
+      return token;
+    }
+
     try {
-      String accessToken = resourceApiProvider.post(
+      token = resourceApiProvider.post(
           new URL(endpoint),
           new HashMap<>(),
-          new String(apiKey));
-      return accessToken.toCharArray();
+          new String(apiKey)).toCharArray();
+      tokenExpiration = getTokenExpiration(token);
+      return token;
     } catch (Exception e) {
       throw new AuthenticationException(e);
     }
+  }
+
+  private Instant getTokenExpiration(char[] token) throws JsonProcessingException {
+    String claims = new String(Base64
+        .getDecoder()
+        .decode(Util.getNode(new String(token), "payload").textValue()));
+    String iat = Util.getNode(claims, "iat").asText();
+
+    return LocalDateTime.ofEpochSecond(Long.parseLong(iat), 0, ZoneOffset.UTC)
+        .plusSeconds(DEFAULT_LIFESPAN_SECONDS)
+        .toInstant(ZoneOffset.UTC);
   }
 
   private void setEndpoint(Credentials credentials) {
