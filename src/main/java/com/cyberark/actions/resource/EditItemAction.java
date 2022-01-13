82cec222e6cf5f98cd12dd8f54da0b32eab40e8f
ehadar <eran.hadar@cyberark.com>
2022-01-13 13:36:52 +0200

    Extract strings

--- src/main/java/com/cyberark/actions/resource/EditItemAction.java
+++ src/main/java/com/cyberark/actions/resource/EditItemAction.java
@@ -9,12 +9,12 @@ import java.util.function.Supplier;
 @SelectionBasedAction
 public abstract class EditItemAction<T extends ResourceModel> extends ActionBase<T> {
   public EditItemAction(Supplier<T> selectedResource) {
-    this(selectedResource, "Edit");
+    this(selectedResource, getString("edit.item.action.text"));
   }
 
   public EditItemAction(Supplier<T> selectedResource, String text) {
     super(text, ActionType.EditItem, selectedResource);
-    putValue(SHORT_DESCRIPTION, "Edit the selected resource");
+    putValue(SHORT_DESCRIPTION, getString("edit.item.action.short.description"));
     putValue(MNEMONIC_KEY, KeyEvent.VK_E);
     setEnabled(false);
   }
