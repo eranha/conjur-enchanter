81423cfe1b47892db9373c9ff324f0ee11e472c4
ehadar <eran.hadar@cyberark.com>
2022-01-14 13:16:37 +0200

    Documentation

--- src/main/java/com/cyberark/controllers/AuthnController.java
+++ src/main/java/com/cyberark/controllers/AuthnController.java
@@ -1,8 +1,13 @@
 package com.cyberark.controllers;
 
-import javax.swing.*;
-
 public interface AuthnController {
+  /**
+   * Execute login process, displays a credentials dialog.
+   */
   void login();
+
+  /**
+   * Logs out the user and clears all data.
+   */
   void logout();
 }
