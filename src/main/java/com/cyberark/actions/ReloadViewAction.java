50dc2c9842fe375d24c6a60e9c953160b40f2f48
ehadar <eran.hadar@cyberark.com>
2022-01-14 16:51:56 +0200

    Add action short description

--- src/main/java/com/cyberark/actions/ReloadViewAction.java
+++ src/main/java/com/cyberark/actions/ReloadViewAction.java
@@ -3,7 +3,7 @@ package com.cyberark.actions;
 import com.cyberark.controllers.ControllerFactory;
 import com.cyberark.views.Icons;
 
-import javax.swing.*;
+import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
@@ -12,8 +12,14 @@ import static com.cyberark.util.Resources.getString;
 
 public class ReloadViewAction extends AbstractAction {
   public ReloadViewAction() {
-    super("Reload", Icons.getInstance().getIcon(Icons.ICON_SPIN, 16, CYBR_BLUE));
-    putValue(SHORT_DESCRIPTION, getString("reload.view.action.text"));
+    super(
+      getString("reload.view.action.text"),
+      Icons.getInstance().getIcon(Icons.ICON_SPIN,
+        16,
+        CYBR_BLUE
+      )
+    );
+    putValue(SHORT_DESCRIPTION, getString("reload.view.action.description"));
     putValue(MNEMONIC_KEY, KeyEvent.VK_R);
   }
 
